c36af2d150c2a90577da39093dc3aaf72ec96ef4
yatsenkonastya <anastasiiayatsenko99@gmail.com>
2023-06-23 02:13:05 +0300

    homework number 11

--- src/FamilyOfCalculator/SimpleCalculator.java
+++
@@ -1,8 +0,0 @@
-package FamilyOfCalculator;
-
-public abstract class SimpleCalculator {
-    public abstract void summ (double a, double b);
-    public abstract void subtraction (double a, double b);
-    public abstract void division (double a, double b);
-    public abstract void multiplication (double a, double b);
-}
